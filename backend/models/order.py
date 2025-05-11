dff80fdc1ff2ec7795c3cb1cfd4500c0d45f2e41
Presktok <160450140+Presktok@users.noreply.github.com>
2025-05-11 12:02:33 +0530

    update

--- backend/models/order.py
+++ backend/models/order.py
@@ -5,6 +5,7 @@ class Order:
     def create_order(order_data):
         return mongo.db.orders.insert_one(order_data)
 
+
     @staticmethod
     def get_orders():
         return list(mongo.db.orders.find())
